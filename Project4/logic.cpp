c7b10d9475c32ccead12626e84aadffbdf9a0466
KharytonchykRaman <157610424+KharytonchykRaman@users.noreply.github.com>
2024-03-16 19:12:05 +0300

    Update logic.cpp

--- Project4/logic.cpp
+++ Project4/logic.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int max_sum_line(int** matrix, int width, int length) {
-
-	int sum = 0;
+	
 	int maxSum = 0;
 	int maxSumIndex = 0;
 
 	for (int i = 0; i < width; i++)
 	{
+		int sum = 0;
 		for (int j = 0; j < length; j++)
 		{
 			sum += matrix[i][j];
